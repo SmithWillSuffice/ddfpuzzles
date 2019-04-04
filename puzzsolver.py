5794c0772ae29b3f0d25311426cf09b29e7b8138
SmithWillSuffice <achrononmaster@gmail.com>
2019-04-04 22:49:48 +1300

    Added docs and edits to docstrings.

--- puzzsolver.py
+++ puzzsolver.py
@@ -12,44 +12,49 @@ from collections import deque
 import re
 
 class PuzzSolver(deque):
-    '''Generic discrete deterministic finite state puzzle solver.  A Puzzle instantiation inherited 
-    from this class must have a current state 'pos' and an iterator defined in 
-    an __iter__() function.  It must also implement a method canonical() which
-    transforms a state 'pos' into a canonical form (equivalence class standard 
-    representative), how you do this is up to you and depends on the particulars
+    '''Generic discrete deterministic finite state puzzle solver.  
+    
+    A Puzzle instantiation inherited from this class must have a current 
+    state 'pos' and an iterator defined in an __iter__() function.  It must 
+    also implement a method canonical() which  transforms a state 'pos' into a 
+    canonical form (equivalence class standard representative), how you do 
+    this is up to you and depends on the particulars
     of your puzzle.
     
     For an example see,
-    './nine_sliding_blocks/nine_sliding_blocks/.py'
+    ``./nine_sliding_blocks/.py``
     or 
-    './jug_fill/jug_fill.py'
+    ``./jug_fill.py``
     
     Should work for any game/puzzle where a give state (or position 'pos') of 
     the game can be specified by a list or string, and a move is a discrete 
     transform of the state.
     
-    Usage:
+    **Usage:**
     
     You need to do something like the following at a minimum. A class derived 
-    from puzzle with methods; isgoal(), canonical(), __iter__() 
+    from puzzle with methods; `isgoal()`, `canonical()`, `__iter__()`.
     
+    **Example:**
+    
+    >>>
     from ddfpuzzles.puzzsolver import PuzzSolver
     class YourGame(PuzzSolver):
        pos = #... initial state of the game
-       
-        def __init__(self,*args,**kwargs):
-            # you could use a kwarg for setting a different initial pos
-            if 'start' in kwargs: self.pos = kwargs['start']
-            if args :
-                self.pos = args[0]
-            
+       #
+       def __init__(self,*args,**kwargs):
+          # you could use a kwarg for setting a different initial pos
+          if 'start' in kwargs: self.pos = kwargs['start']
+          if args :
+             self.pos = args[0]
+             #
        def isgoal(self):
           return #... true iff the current state is a solution
                  # could be a simple '==' match or a regexp match.
-                 
+                 #
        def canonical(self):
           # convert every position in the game to a standard equivalent form.
-          
+          #
        def __iter__(self):
           # generates every possible move of the game from pos with yield 
           # statements.
@@ -60,7 +65,7 @@ class PuzzSolver(deque):
           # You might also want to raise a custom exception if no moves are 
           # possible, which might print "No solution exists." or 
           # f"Cannot move from position {pos}." an then exits.
-       
+          #
     if __name__ == '__main__':
         from pprint import pprint
         puzz = YourGame()
